aa3e74f5e0c4fe1a1c718ee5b50c3b2eaf5aafc4
OParnu <110712221+OParnu@users.noreply.github.com>
2024-08-15 22:04:53 +0700

    Login Page Version 0.1
    ยังไม่ทำ @click
    bug alert

---
+++ Kradan-Kanban_Front-End/src/components/LogicPage.vue
@@ -0,0 +1,80 @@
+<script setup>
+import { ref, watch } from "vue";
+const userData = ref({
+  username: "",
+  password: "",
+});
+
+const dataValidation = ref(false);
+
+watch(userData.value, () => {
+  if (userData.value.username !== "" && userData.value.password !== "")
+    dataValidation.value = false;
+  else dataValidation.value = true;
+});
+
+</script>
+
+<template>
+  <div
+    class="flex flex-col justify-center gap-3 items-center h-screen bg-slate-600 bg-transparent"
+  >
+    <div
+      class="itbkk-message bg-red-100 border border-red-400 text-red-700 px-10 py-6 rounded relative"
+      role="alert"
+      v-show="dataValidation"
+    >
+      <strong class="font-bold">Error: </strong>
+      <span class="block sm:inline">Username or Password is incorrect</span>
+      <span class="absolute top-0 bottom-0 right-0">
+        <svg
+          class="fill-current h-6 w-6 text-red-500"
+          @click="dataValidation = false"
+          role="button"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <title>Close</title>
+          <path
+            d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
+          />
+        </svg>
+      </span>
+    </div>
+
+    <div class="bg-white p-6 rounded-lg shadow-md w-full max-w-sm">
+      <h2 class="text-2xl font-bold mb-4">Welcome to ITBKK-KP2</h2>
+
+      <div class="mb-4">
+        <span class="block text-sm font-medium text-gray-700"
+          >Username</span
+        >
+        <input
+          type="text"
+          v-model="userData.username"
+          class="itbkk-username input input-bordered w-full mt-1"
+          maxlength="50"
+        />
+      </div>
+      <div class="mb-6">
+        <span class="block text-sm font-medium text-gray-700"
+          >Password</span
+        >
+        <input
+          type="password"
+          v-model="userData.password"
+          class="itbkk-password input input-bordered w-full mt-1"
+          maxlength="14"
+        />
+      </div>
+      <button
+        class="itbkk-button-signin w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        :disabled="!dataValidation"
+      >
+        Login
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
