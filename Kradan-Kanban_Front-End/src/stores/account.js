d40ff3f66e440b3c80671cadd6b3651424eefa3d
Test-65130500047 <110713141+DekchaiHeart@users.noreply.github.com>
2024-08-25 01:36:08 +0700

    Update Username and Add logout bth

--- Kradan-Kanban_Front-End/src/stores/account.js
+++ Kradan-Kanban_Front-End/src/stores/account.js
@@ -2,16 +2,19 @@ import { defineStore } from "pinia";
 
 export const useAccountStore = defineStore("account", {
   state: () => ({
-    tokenDetail: {},
+    tokenDetail: JSON.parse(localStorage.getItem("tokenDetail")) || {},
   }),
   getters: {
-    // Extract the name from the decoded token details
     userName: (state) => state.tokenDetail.name || "",
   },
   actions: {
-    // Action to set the tokenDetail directly
     setTokenDetail(tokenDetail) {
       this.tokenDetail = tokenDetail;
+      localStorage.setItem("tokenDetail", JSON.stringify(tokenDetail));
+    },
+    clearTokenDetail() {
+      this.tokenDetail = {};
+      localStorage.removeItem("tokenDetail");
     },
   },
 });
