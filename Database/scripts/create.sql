65d74760c3b97472b74c32ece777c07794052b66
Jhatniphat Sarakal <52996903+Jhatniphat@users.noreply.github.com>
2024-09-16 23:39:01 +0700

    Update create.sql

--- Database/scripts/create.sql
+++ Database/scripts/create.sql
@@ -1,65 +1,119 @@
-set character_set_client='utf8';
-set character_set_connection='utf8';
-set character_set_database='utf8';
-set character_set_results='utf8';
-set character_set_server='utf8';
+-- MySQL Workbench Forward Engineering
 
-DROP SCHEMA IF EXISTS `intergrate-kp-2` ;
-CREATE SCHEMA IF NOT EXISTS `intergrate-kp-2` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `intergrate-kp-2` ;
+DROP SCHEMA IF EXISTS `intergrate-kp-2`;
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema intergrate-kp-2
+-- -----------------------------------------------------
 
--- Table `intergrate-kp-2`.`status`
-DROP TABLE IF EXISTS `intergrate-kp-2`.`status` ;
-CREATE TABLE `intergrate-kp-2`.`status` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `statusName` VARCHAR(50) NOT NULL,
-  `statusDescription` VARCHAR(200) NULL,
-  `createdOn` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedOn` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id` ,`statusName`),
-  UNIQUE INDEX `statusName_UNIQUE` (`statusName` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+-- -----------------------------------------------------
+-- Schema intergrate-kp-2
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `intergrate-kp-2` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `intergrate-kp-2`;
 
--- Table `intergrate-kp-2`.`task`
-DROP TABLE IF EXISTS `intergrate-kp-2`.`task` ;
-CREATE TABLE IF NOT EXISTS `intergrate-kp-2`.`task` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(100) NOT NULL,
-  `description` VARCHAR(500) NULL DEFAULT NULL,
-  `assignees` VARCHAR(30) NULL DEFAULT NULL,
-  `status` VARCHAR(50) NOT NULL,
-  `createdOn` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedOn` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`status`)
-        REFERENCES `intergrate-kp-2`.`status`(`statusName`)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE
-        )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
--- Table `intergrate-kp-2`.`limitSettings`
-DROP TABLE IF EXISTS `intergrate-kp-2`.`limitSettings`;
-CREATE TABLE IF NOT EXISTS `intergrate-kp-2`.`limitSettings` (
-  `limit` INT NOT NULL DEFAULT 10,
-  `isEnable` BOOLEAN NOT NULL DEFAULT TRUE
+SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0;
+SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0;
+SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE =
+        'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+-- -----------------------------------------------------
+-- Table `intergrate-kp-2`.`board`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `intergrate-kp-2`.`board`
+(
+    `boardId`   VARCHAR(10)  NOT NULL,
+    `boardName` VARCHAR(120) NOT NULL,
+    `userId`    VARCHAR(36)  NULL,
+    PRIMARY KEY (`boardId`)
 )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
 
-insert into `intergrate-kp-2`.`status` (`statusName` , `statusDescription`) value ("No Status" , "The default status");
-insert into `intergrate-kp-2`.`status` (`statusName`) value ("To Do");
-insert into `intergrate-kp-2`.`status` (`statusName` , `statusDescription`) value ("Doing" , "Being worked on");
-insert into `intergrate-kp-2`.`status` (`statusName` , `statusDescription`) value ("Done" , "Finished");
 
-insert into `intergrate-kp-2`.`limitSettings` (`limit` , `isEnable`) value (10 ,TRUE);
-ALTER DATABASE `intergrate-kp-2` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- -----------------------------------------------------
+-- Table `intergrate-kp-2`.`limitsettings`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `intergrate-kp-2`.`limitsettings`
+(
+    `id`         INT         NOT NULL AUTO_INCREMENT,
+    `limit`      INT         NOT NULL DEFAULT '10',
+    `isEnable`   TINYINT(1)  NOT NULL DEFAULT '1',
+    `ls_boardId` VARCHAR(10) NOT NULL,
+    INDEX `boardId_idx` (`ls_boardId` ASC) VISIBLE,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `ls_boardId`
+        FOREIGN KEY (`ls_boardId`)
+            REFERENCES `intergrate-kp-2`.`board` (`boardId`)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+)
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
 
-DROP USER IF EXISTS 'dev'@'%';
-CREATE USER 'dev'@'%' IDENTIFIED BY 'ip23kp2mysql';
-GRANT ALL ON `intergrate-kp-2`.* TO 'dev'@'%';
+
+-- -----------------------------------------------------
+-- Table `intergrate-kp-2`.`status`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `intergrate-kp-2`.`status`
+(
+    `id`                INT          NOT NULL AUTO_INCREMENT,
+    `statusName`        VARCHAR(50)  NOT NULL,
+    `statusDescription` VARCHAR(200) NULL DEFAULT NULL,
+    `createdOn`         DATETIME     NULL DEFAULT CURRENT_TIMESTAMP,
+    `updatedOn`         DATETIME     NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `st_boardId`        VARCHAR(10)  NOT NULL,
+    PRIMARY KEY (`id` ,`statusName` , `st_boardId`),
+    UNIQUE INDEX `statusName_UNIQUE` (`statusName` ASC , `st_boardId` ASC) VISIBLE,
+    INDEX `boardId_idx` (`st_boardId` ASC) VISIBLE,
+    CONSTRAINT `st_boardId`
+        FOREIGN KEY (`st_boardId`)
+            REFERENCES `intergrate-kp-2`.`board` (`boardId`)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+)
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 10
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `intergrate-kp-2`.`task`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `intergrate-kp-2`.`task`
+(
+    `id`          INT          NOT NULL AUTO_INCREMENT,
+    `title`       VARCHAR(100) NOT NULL,
+    `description` VARCHAR(500) NULL     DEFAULT NULL,
+    `assignees`   VARCHAR(30)  NULL     DEFAULT NULL,
+    `status`      VARCHAR(50)  NOT NULL,
+    `createdOn`   DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updatedOn`   DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `tk_boardId`  VARCHAR(10)  NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `status` (`status` ASC) VISIBLE,
+    INDEX `boardId_idx` (`tk_boardId` ASC) VISIBLE,
+    CONSTRAINT `task_ibfk_1`
+        FOREIGN KEY (`status`)
+            REFERENCES `intergrate-kp-2`.`status` (`statusName`)
+            ON DELETE RESTRICT
+            ON UPDATE CASCADE,
+    CONSTRAINT `tk_boardId`
+        FOREIGN KEY (`tk_boardId`)
+            REFERENCES `intergrate-kp-2`.`board` (`boardId`)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+)
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 20
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE = @OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS;
